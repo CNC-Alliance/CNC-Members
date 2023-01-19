0636f3ab3eb8cfca9bc3adb66f2565f7dec0d0b3
Brolloks <94197082+Brolloks@users.noreply.github.com>
2023-01-19 12:47:49 +0000

    Update membersImport.py

--- membersImport.py
+++ membersImport.py
@@ -3,18 +3,14 @@ import os
 
 filepath = os.path.join(os.path.dirname(__file__),"cnc-alliance-members-registration")
 
-# Load the JSON data from the file
 with open(filepath) as json_file:
     data = json.load(json_file)
 
-# Create a new list to store the poolIds of the entries that have "Active" or "ISPO" in the membershipType field
 pool_ids = {}
 
-# Iterate through the entries in the JSON data
 for key, value in data.items():
     if value["membershipType"] in ["Active", "ISPO"]:
         pool_ids[value["ticker"]] = value["poolId"]
 
-# Write the list of poolIds to a new JSON file
 with open("pool_ids.json", "w") as outfile:
     json.dump(pool_ids, outfile)
