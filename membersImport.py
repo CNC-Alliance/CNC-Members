e4e6809a37feeb78fe0e3f1f89eb0f07588107e3
Brolloks <94197082+Brolloks@users.noreply.github.com>
2023-01-19 14:01:16 +0000

    Update membersImport.py

--- membersImport.py
+++ membersImport.py
@@ -12,7 +12,7 @@ pool_ids = []
 for key in data["adapools"]["members"].keys():
     v = data["adapools"]["members"][key]
     if v["membershipType"] in ["Active", "ISPO"]:
-        pool_ids.append({"ticker" = v["ticker"], "poolId" = v["poolId"]})
+        pool_ids.append({"ticker" : v["ticker"], "poolId" : v["poolId"]})
        
 pool_ids = sorted(pool_ids, key = lambda x : x["ticker"])
 
