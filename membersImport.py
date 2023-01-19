f805bca51840d336af131ad60149f2c85436d96e
Brolloks <94197082+Brolloks@users.noreply.github.com>
2023-01-19 13:08:16 +0000

    Update membersImport.py

--- membersImport.py
+++ membersImport.py
@@ -1,7 +1,7 @@
 import json
 import requests
 
-json_url = "https://github.com/Brolloks/CNC-Members/blob/main/cnc-alliance-members-registration"
+json_url = "https://raw.githubusercontent.com/Brolloks/CNC-Members/main/cnc-alliance-members-registration"
 
 response = requests.get(json_url)
 print(response.text)
