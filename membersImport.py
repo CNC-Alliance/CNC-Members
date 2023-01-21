bae92a8372c77d80f1627c45f3e1cc2dedc38065
Brolloks <94197082+Brolloks@users.noreply.github.com>
2023-01-21 20:19:33 +0000

    Update membersImport.py

--- membersImport.py
+++ membersImport.py
@@ -8,10 +8,15 @@ print("version 1.1")
 data = json.loads(response.text)
 
 pool_ids = []
+seen_pool_ids = set()
 for key in data["adapools"]["members"].keys():
     v = data["adapools"]["members"][key]
     if v["membershipType?"] in ["Active", "ISPO"]:
-        pool_ids.append({"ticker" : v["ticker"], "poolId" : v["poolId"]})
+        if v["poolId"] in seen_pool_ids:
+            pool_ids.append({"ticker" : v["ticker"] + " DUPLICATE", "poolId" : v["poolId"]})
+        else:
+            pool_ids.append({"ticker" : v["ticker"], "poolId" : v["poolId"]})
+            seen_pool_ids.add(v["poolId"])
        
 pool_ids = sorted(pool_ids, key = lambda x : x["ticker"])
 
@@ -19,4 +24,3 @@ with open("CNC_Active_ISPO_POOL_CHECK.json", "w") as outfile:
     json.dump(pool_ids, outfile, indent = 2)
 
 print("Pool IDs successfully written to CNC_Active_ISPO_POOL_CHECK.json")
-print(response.text)
