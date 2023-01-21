69cd6b1657cbe07615c60d6871ce15ac72dcc831
Brolloks <94197082+Brolloks@users.noreply.github.com>
2023-01-21 18:53:29 +0000

    Update extract_poolId.py

--- extract_poolId.py
+++ extract_poolId.py
@@ -1,11 +1,14 @@
 import json
 
+# load the output file from the first script
 with open("CNC_Active_ISPO_pool_ids.json", "r") as infile:
     data = json.load(infile)
 
+# extract the poolId
 pool_ids = [d["poolId"] for d in data]
 
+# write the poolIds to a new output file
 with open("CNC_ISPO_OFFICIAL.json", "w") as outfile:
-    json.dump(pool_ids, outfile)
+    json.dump(pool_ids, outfile, indent = 2)
 
-print("CNC_ISPO_OFFICIAL.json created successfully")
+print("Pool IDs successfully written to CNC_ISPO_OFFICIAL.json")
